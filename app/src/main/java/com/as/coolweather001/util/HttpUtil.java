902ea482b65c170ae888e4bf9ab82f5cbf6ed01f
Dust <2283025413@qq.com>
2018-02-22 18:50:59 +0800

    完成遍历省市县

---
+++ app/src/main/java/com/as/coolweather001/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.as.coolweather001.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by pc on 2018/2/22.
+ */
+
+public class HttpUtil{
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
